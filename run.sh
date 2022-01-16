afd71941a9c01a03a6bb9128783b422de0deed19
truongan@dell-xps-13-9343 <truongan@dell-xps-13-9343>
2022-01-17 05:57:40 +0700

    add run command

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 # for i in /dev/nvidia*; do  echo -n " --device $i" ; done
 # docker run -it --rm --gpus all --device /dev/nvidia0  --device /dev/nvidia-modeset --device /dev/nvidiactl -u $(id -u):$(id -g) -v `pwd`:/current truongan/uit-vsum:1.0 bash
+# docker run -it --rm  -u $(id -u):$(id -g) -v `pwd`:/current truongan/uit-vsum:1.0 bash
